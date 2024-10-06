d2d28c7bcf7406ccb2177e0a08c7a32bcd53d515
Elton Silva <eltoncarsilva73@gmail.com>
2024-10-06 19:02:44 -0300

    fix opengl rendering

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sdl2_gamebase_project
+
+pacman -S mingw-w64-ucrt-x86_64-SDL2
+pacman -S mingw-w64-ucrt-x86_64-glm
+pacman -S mingw-w64-ucrt-x86_64-glew
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <array>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
-#include <GL/gl.h>
 
 using namespace std;
 
@@ -24,7 +26,17 @@ int main(int argc, char **args)
     {
         return 1;
     }
-    ClearRender();
+
+    std::array<glm::vec3, 3> Triangle = {
+        glm::vec3{-1.0f, -1.0f, 0.0f},
+        glm::vec3{1.0f, -1.0f, 0.0f},
+        glm::vec3{0.0f, 1.0f, 0.0f},
+    };
+    GLuint VertexBuffer;
+
+    glGenBuffers(1, &VertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Triangle), Triangle.data(), GL_STATIC_DRAW);
 
     bool running = true;
     Uint32 frameStart;
@@ -34,9 +46,16 @@ int main(int argc, char **args)
     {
         frameStart = SDL_GetTicks();
 
+        ClearRender();
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+        glVertexAttribPointer(0, Triangle.size(), GL_FLOAT, GL_FALSE, 0, nullptr);
+        glDrawArrays(GL_TRIANGLES, 0, Triangle.size());
+        glDisableVertexAttribArray(0);
+
         Update();
         Render();
-        ClearRender();
 
         frameTime = SDL_GetTicks() - frameStart;
         if (frameDelay > frameTime)
@@ -51,7 +70,6 @@ int main(int argc, char **args)
 
 bool InitWindow()
 {
-
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         cout << "Error initializing SDL: " << SDL_GetError() << endl;
@@ -71,6 +89,34 @@ bool InitWindow()
         cout << "Error creating renderer: " << SDL_GetError() << endl;
         return false;
     }
+
+    if (SDL_GL_SetSwapInterval(1) < 0)
+    {
+        printf("Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
+    }
+
+    if (glewInit() != GLEW_OK)
+    {
+        cout << "Erro ao inicializar o GLEW" << endl;
+        return false;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    GLint GLMajorVersion = 0;
+    GLint GLMinorVersion = 0;
+
+    glGetIntegerv(GL_MAJOR_VERSION, &GLMajorVersion);
+    glGetIntegerv(GL_MINOR_VERSION, &GLMinorVersion);
+
+    cout << "OpenGL version " << GLMajorVersion << "." << GLMinorVersion << endl;
+
+    glViewport(0, 0, 800, 800);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
     return true;
 }
@@ -92,14 +138,12 @@ void Update()
 
 void Render()
 {
+    SDL_GL_SwapWindow(window);
 }
 
 void ClearRender()
 {
-    glViewport(0, 0, 800, 800);
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    SDL_GL_SwapWindow(window);
 }
 
 void KillWindow()
