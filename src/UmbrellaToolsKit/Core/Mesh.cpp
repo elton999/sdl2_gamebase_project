8d2dbf7716a0d6f0d1d9e8504c71aa1d83d92aa7
Elton Silva <eltoncarsilva73@gmail.com>
2025-11-05 23:19:18 -0300

    added class mesh

---
+++ src/UmbrellaToolsKit/Core/Mesh.cpp
@@ -0,0 +1,25 @@
+#include "Mesh.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+void Mesh::LoadData(std::vector<glm::vec3> &vertices)
+{
+    _mVertices = &vertices;
+    glGenVertexArrays(1, &mVAO);
+    glGenBuffers(1, &mVBO);
+    glBindVertexArray(mVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), vertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void Mesh::Draw()
+{
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    // glDisableVertexAttribArray(0);
+}
---
+++ src/UmbrellaToolsKit/Core/Mesh.h
@@ -0,0 +1,20 @@
+#ifndef _MESH_H_
+#define _MESH_H_
+
+#include <iostream>
+#include <vector>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+
+class Mesh
+{
+private:
+    unsigned int mVBO, mVAO;
+    std::vector<glm::vec3> *_mVertices;
+
+public:
+    void LoadData(std::vector<glm::vec3> &vertices);
+    void Draw();
+};
+
+#endif // _MESH_H_
