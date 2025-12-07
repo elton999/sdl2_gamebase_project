1c0833f42204c86e47fa4c840659e3002b83d7ef
Elton Silva <eltoncarsilva73@gmail.com>
2025-12-07 15:13:40 -0300

    added logic to support color and text coord

--- src/UmbrellaToolsKit/Core/Mesh.h
+++ src/UmbrellaToolsKit/Core/Mesh.h
@@ -12,6 +12,9 @@ class Mesh
 private:
     unsigned int mVBO, mVAO;
     std::vector<glm::vec3> *_mVertices;
+    std::vector<glm::vec3> *_mVerticesColors;
+    std::vector<glm::vec2> *_mTexCoords;
+
     unsigned int _mVertexCount;
     Shader *_mShader;
     bool _mHasShader = false;
@@ -21,6 +24,7 @@ public:
     Shader *GetShader();
 
     void LoadData(std::vector<glm::vec3> &vertices);
+    void LoadData(std::vector<glm::vec3> &vertices, std::vector<glm::vec3> &colors, std::vector<glm::vec2> &texCoords);
     void Draw();
 };
 
