51714fdbf2a291d81cc67e832fcddd8595dbb609
Elton Silva <eltoncarsilva73@gmail.com>
2025-12-07 18:45:35 -0300

    created sprite class

--- src/UmbrellaToolsKit/Core/Sprite.h
+++ src/UmbrellaToolsKit/Core/Sprite.h
@@ -1,24 +1,64 @@
 #ifndef _SPRITE_H_
 #define _SPRITE_H_
 
-#include "SquareMesh.h"
+#include "Mesh.h"
+#include "Texture2D.h"
 
-class Sprite : public SquareMesh
+class Sprite : public Mesh
 {
 public:
-    Sprite() : SquareMesh()
+    Sprite()
     {
-        Shader shader(
+
+        std::vector<glm::vec3> square =
+            {
+                glm::vec3{0.0f, 1.0f, 0.0f},
+                glm::vec3{1.0f, 1.0f, 0.0f},
+                glm::vec3{0.0f, 0.0f, 0.0f},
+
+                glm::vec3{1.0f, 1.0f, 0.0f},
+                glm::vec3{1.0f, 0.0f, 0.0f},
+                glm::vec3{0.0f, 0.0f, 0.0f},
+            };
+
+        std::vector<glm::vec2> texCoords =
+            {
+                glm::vec2{0.0f, 1.0f},
+                glm::vec2{1.0f, 1.0f},
+                glm::vec2{0.0f, 0.0f},
+
+                glm::vec2{1.0f, 1.0f},
+                glm::vec2{1.0f, 0.0f},
+                glm::vec2{0.0f, 0.0f},
+            };
+
+        std::vector<glm::vec3> colors =
+            {
+                glm::vec3{1.0f, 0.0f, 0.0f},
+                glm::vec3{0.0f, 1.0f, 0.0f},
+                glm::vec3{0.0f, 0.0f, 1.0f},
+
+                glm::vec3{1.0f, 1.0f, 0.0f},
+                glm::vec3{0.0f, 1.0f, 1.0f},
+                glm::vec3{1.0f, 0.0f, 1.0f},
+            };
+
+        this->LoadData(square, colors, texCoords);
+
+        const char *vertexCode =
             "#version 330 core\n"
-            "layout(location = 0) in vec4 vertex; // <vec2 position, vec2 texCoords>\n"
+            "layout(location = 0) in vec3 position;\n"
+            "layout(location = 1) in vec3 vertexColor;\n"
+            "layout(location = 2) in vec2 texCoords;\n"
             "out vec2 TexCoords;\n"
             "uniform mat4 model;\n"
             "uniform mat4 projection;\n"
             "void main() {\n"
-            "    TexCoords = vertex.zw;\n"
-            "    gl_Position = projection * model * vec4(vertex.xy, 0.0, 1.0);\n"
-            "}\n\0",
+            "    TexCoords = texCoords;\n"
+            "    gl_Position = projection * model * vec4(position, 1.0);\n"
+            "}\n";
 
+        const char *fragmentCode =
             "#version 330 core\n"
             "in vec2 TexCoords;\n"
             "out vec4 color;\n"
@@ -26,11 +66,14 @@ public:
             "uniform vec3 spriteColor;\n"
             "void main()\n"
             "{\n"
-            "        color = vec4(spriteColor, 1.0) * texture(image, TexCoords);\n"
-            "}\n\0");
-        ;
-        this->SetShader(shader);
+            "    color = vec4(spriteColor, 1.0) * texture(image, TexCoords);\n"
+            "}\n";
+
+        Shader *shader = new Shader(vertexCode, fragmentCode);
+        this->SetShader(*shader);
     }
+
+    void DrawSprite(Texture2D &texture, glm::vec2 position, glm::vec2 size, float rotate, glm::vec3 color);
 };
 
 #endif // _SPRITE_H_
